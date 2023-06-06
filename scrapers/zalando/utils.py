412778d1e16dee29174ecad776834cda7942954e
Igor Wlodarczyk <wlodar.igor@gmail.com>
2023-06-06 20:38:56 +0200

    added docstring

--- scrapers/zalando/utils.py
+++ scrapers/zalando/utils.py
@@ -16,6 +16,26 @@ def get_currency(url: str) -> str:
 
 
 def detect_shoe_sizes_and_parse(sizes: Union[list, str]) -> list:
+    """
+        Detects shoe sizes and parses them from EU measurement to US measurement.
+
+        :param sizes: The sizes to be detected and parsed. It can be either a list of strings or a single string.
+        :return: The parsed sizes as a list of strings.
+
+        If the input `sizes` is a string, it is parsed into a list of sizes using the `parse_sizes` function.
+        Each size in the list is converted from EU measurement to US measurement using the `convert_size_eu_to_us` function.
+        The converted sizes are stored in a new list `parsed_sizes`, which is returned as the result.
+
+        If any error occurs during the conversion (e.g., invalid input size), the original sizes are returned as is.
+
+        Example:
+        >>> detect_shoe_sizes_and_parse(["42.5", "38", "39.5"])
+        ['9.0', '5.0', '6.5']
+        >>> detect_shoe_sizes_and_parse("41.5 37 40")
+        ['8.5', '5.0', '7.0']
+        >>> detect_shoe_sizes_and_parse(["45.5", "abc", "39"])
+        ['11.5', 'abc', '6.0']
+        """
     sizes = parse_sizes(sizes)
     parsed_sizes = []
     try:
